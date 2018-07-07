4d424f0ca423e35d03228c6f81ba49ef33928154
Larry Petroski <larry.petroski@yahoo.com>
2018-07-07 18:13:09 -0500

    Chapter 15 Completed

--- MarbleMaze/GameViewController.swift
+++ MarbleMaze/GameViewController.swift
@@ -20,6 +20,12 @@ class GameViewController: UIViewController {
     var scnView:SCNView!
     var scnScene:SCNScene!
     var ballNode:SCNNode!
+    var cameraNode:SCNNode!
+    var cameraFollowNode:SCNNode!
+    var lightFollowNode:SCNNode!
+    var game = GameHelper.sharedInstance
+    var motion = CoreMotionHelper()
+    var motionForce = SCNVector3(x:0, y:0, z:0)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,13 +33,15 @@ class GameViewController: UIViewController {
         setupScene()
         setupNodes()
         setupSounds()
+        
+        resetGame()
     }
     
     func setupScene() {
         scnView = self.view as! SCNView
         scnView.delegate = self
-        scnView.allowsCameraControl = true
-        scnView.showsStatistics = true
+        //scnView.allowsCameraControl = true
+        //scnView.showsStatistics = true
         
         scnScene = SCNScene(named: "art.scnassets/game.scn")
         scnView.scene = scnScene
@@ -43,17 +51,132 @@ class GameViewController: UIViewController {
     func setupNodes() {
         ballNode = scnScene.rootNode.childNode(withName: "ball", recursively: true)!
         ballNode.physicsBody?.contactTestBitMask = CollisionCategoryPillar | CollisionCategoryCrate | CollisionCategoryPearl
+        
+        cameraNode = scnScene.rootNode.childNode(withName: "camera", recursively: true)!
+        let constraint = SCNLookAtConstraint(target: ballNode)
+        cameraNode.constraints = [constraint]
+        constraint.isGimbalLockEnabled = true
+        
+        cameraFollowNode = scnScene.rootNode.childNode(withName: "follow_camera", recursively: true)!
+        cameraNode.addChildNode(game.hudNode)
+        lightFollowNode = scnScene.rootNode.childNode(withName: "follow_light", recursively: true)!
     }
     
     func setupSounds() {
+        game.loadSound(name: "GameOver", fileNamed: "GameOver.wav")
+        game.loadSound(name: "Powerup", fileNamed: "Powerup.wav")
+        game.loadSound(name: "Reset", fileNamed: "Reset.wav")
+        game.loadSound(name: "Bump", fileNamed: "Bump.wav")
     }
     
     override var shouldAutorotate: Bool {return false}
     override var prefersStatusBarHidden: Bool {return true}
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if game.state == GameStateType.tapToPlay {
+            playGame()
+        }
+    }
+    
+    func playGame() {
+        game.state = GameStateType.playing
+        cameraFollowNode.eulerAngles.y = 0
+        cameraFollowNode.position = SCNVector3Zero
+        replenishLife()
+    }
+    
+    func resetGame() {
+        game.state = GameStateType.tapToPlay
+        game.playSound(node: ballNode, name: "Reset")
+        ballNode.physicsBody!.velocity = SCNVector3Zero
+        ballNode.position = SCNVector3(x:0, y:10,z:0)
+        cameraFollowNode.position = ballNode.position
+        lightFollowNode.position = ballNode.position
+        scnView.isPlaying = true
+        game.reset()
+    }
+    
+    func testForGameOver() {
+        if ballNode.presentation.position.y < -5 {
+            game.state = GameStateType.gameOver
+            game.playSound(node: ballNode, name: "GameOver")
+            ballNode.runAction(SCNAction.waitForDurationThenRunBlock(duration: 5) {
+                (node:SCNNode!) -> Void in
+                self.resetGame()})
+        }
+    }
+    
+    func updateMotionControl() {
+        if game.state == GameStateType.playing {
+            motion.getAccelerometerData(interval: 0.1) { (x,y,z) in
+                self.motionForce = SCNVector3(x: Float(x) * 0.05, y: 0, z: Float(y+0.8) * -0.05)
+            }
+            ballNode.physicsBody!.velocity += motionForce
+        }
+    }
+    
+    func replenishLife() {
+        let material = ballNode.geometry!.firstMaterial!
+        
+        SCNTransaction.begin()
+        SCNTransaction.animationDuration = 1.0
+        
+        material.emission.intensity = 1.0
+        
+        SCNTransaction.commit()
+        
+        game.score += 1
+        game.playSound(node: ballNode, name: "Powerup")
+    }
+    
+    func diminishLife() {
+        let material = ballNode.geometry!.firstMaterial!
+        
+        if material.emission.intensity > 0 {
+            material.emission.intensity -= 0.001
+        } else {
+            resetGame()
+        }
+    }
+    
+    func updateCameraAndLights() {
+        let lerpX = (ballNode.presentation.position.x - cameraFollowNode.position.x) * 0.01
+        let lerpY = (ballNode.presentation.position.y - cameraFollowNode.position.y) * 0.01
+        let lerpZ = (ballNode.presentation.position.z - cameraFollowNode.position.z) * 0.01
+        
+        cameraFollowNode.position.x += lerpX
+        cameraFollowNode.position.y += lerpY
+        cameraFollowNode.position.z += lerpZ
+        
+        lightFollowNode.position = cameraFollowNode.position
+        
+        if game.state == GameStateType.tapToPlay {
+            cameraFollowNode.eulerAngles.y += 0.005
+        }
+    }
+    
+    func updateHUD() {
+        switch game.state {
+        case .playing:
+            game.updateHUD()
+        case .gameOver:
+            game.updateHUD(s: "-GAME OVER-")
+        case .tapToPlay:
+            game.updateHUD(s: "-TAP TO PLAY-")
+        }
+    }
 }
 
 extension GameViewController:SCNSceneRendererDelegate {
     func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+        updateHUD()
+        updateMotionControl()
+        updateCameraAndLights()
+        
+        if game.state == GameStateType.playing {
+            testForGameOver()
+            diminishLife()
+        }
     }
 }
 
@@ -67,11 +190,13 @@ extension GameViewController:SCNPhysicsContactDelegate {
         }
         
         if contactNode.physicsBody?.categoryBitMask == CollisionCategoryPearl {
+            replenishLife()
             contactNode.isHidden = true
             contactNode.runAction(SCNAction.waitForDurationThenRunBlock(duration: 30) {(node:SCNNode!) -> Void in node.isHidden = false})
         }
         
         if contactNode.physicsBody?.categoryBitMask == CollisionCategoryPillar || contactNode.physicsBody?.categoryBitMask == CollisionCategoryCrate {
+            game.playSound(node: ballNode, name: "Bump")
         }
     }
 }
